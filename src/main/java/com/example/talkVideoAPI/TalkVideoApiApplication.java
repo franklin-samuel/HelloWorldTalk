a5e9f97f471a96bf2dbf2827036553b04c8fc69a
Samuel Franklin <samuelfranklinff@gmail.com>
2025-08-27 23:25:26 -0300

    first: Banco de dados e Oauth2 configurado

---
+++ src/main/java/com/example/talkVideoAPI/TalkVideoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.talkVideoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkVideoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkVideoApiApplication.class, args);
+	}
+
+}
